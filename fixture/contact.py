c02c399595d139f4f11f205b09a66be65a36800b
sash <softester@yandex.ru>
2021-08-11 21:46:11 +0300

    Реализованы проверки в скрипте решения квадратного уравнения

--- fixture/contact.py
+++ fixture/contact.py
@@ -30,6 +30,7 @@ class ContactHelper:
         # submit deletion
         wd.find_element_by_xpath("//input[@value='Delete']").click()
         wd.switch_to_alert().accept()
+        wd.find_element_by_link_text("home")
         wd.find_element_by_link_text("home").click()
 
     def edit_first_contact(self, contact):
