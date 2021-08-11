dd42666b2a5a8091d7357bafaf00eaa373702edd
sash <softester@yandex.ru>
2021-08-11 23:51:49 +0300

    выделен метод выбора первой группы

--- fixture/group.py
+++ fixture/group.py
@@ -13,6 +13,12 @@ class GroupHelper:
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
+        self.fill_group_form(group)
+        # submit group creation
+        wd.find_element_by_name("submit").click()
+        self.return_to_groups_page()
+
+    def fill_group_form(self, group):
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name) if group.name is not None else ""
@@ -22,9 +28,6 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer) if group.footer is not None else ""
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-        self.return_to_groups_page()
 
     def open_groups_page(self):
         wd = self.app.wd
@@ -33,27 +36,23 @@ class GroupHelper:
     def delete_first_group(self):
         wd = self.app.wd
         self.open_groups_page()
-        # select first group
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_group()
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
 
+    def select_first_group(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+
     def edit_first(self, group):
         wd = self.app.wd
         self.open_groups_page()
+        self.select_first_group()
         # open edit form
         wd.find_element_by_name("edit").click()
         # change data
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name) if group.name is not None else ""
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header) if group.header is not None else ""
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer) if group.footer is not None else ""
+        self.fill_group_form(group)
         # submit group creation
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
