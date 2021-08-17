a2cd042183ddecf97ca87f6b597476042b020326
sash <softester@yandex.ru>
2021-08-17 12:13:27 +0300

    Добавлено получение списка групп и проверка списков при создании группы

--- model/group.py
+++ model/group.py
@@ -1,6 +1,7 @@
 class Group:
 
-    def __init__(self, name=None, header=None, footer=None):
+    def __init__(self, id=None, name=None, header=None, footer=None):
+        self.id = id
         self.name = name
         self.header = header
         self.footer = footer
