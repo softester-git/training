242beb58d6a4874e4abcdbb63093c19509ac6068
sash <softester@yandex.ru>
2021-08-17 16:43:45 +0300

    Добавлено получение списка групп и проверка списков при редактировании группы

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -4,4 +4,7 @@ from model.group import Group
 def test_edit_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="Group for edit"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first(Group(name="new_group_name", header="new_group_header", footer="new_group_footer"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
