db54c6b631f303dfd416af717b8d4310e7134d48
sash <softester@yandex.ru>
2021-08-18 10:23:54 +0300

    Добавлена случайность при удалении группы

--- fixture/group.py
+++ fixture/group.py
@@ -35,9 +35,12 @@ class GroupHelper:
             wd.find_element_by_link_text("groups").click()
 
     def delete_first_group(self):
+        self.delete_group_by_index(0)
+
+    def delete_group_by_index(self, index):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
@@ -46,6 +49,10 @@ class GroupHelper:
     def select_first_group(self):
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
+
+    def select_group_by_index(self, index):
+        wd = self.app.wd
+        wd.find_elements_by_name("selected[]")[index].click()
 
     def edit_first(self, group):
         wd = self.app.wd
--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,5 +1,17 @@
 from model.group import Group
+from random import randrange
 
+
+def test_delete_some_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="Group for delete"))
+    old_groups = app.group.get_group_list()
+    index = randrange(len(old_groups))
+    app.group.delete_group_by_index(index)
+    assert len(old_groups) - 1 == app.group.count()
+    new_groups = app.group.get_group_list()
+    old_groups[index:index+1] = []
+    assert old_groups == new_groups
 
 def test_delete_first_group(app):
     if app.group.count() == 0:
