d71f920bc353b03ba70ceea25110e8708ab6afbb
sash <softester@yandex.ru>
2021-08-18 08:25:15 +0300

    Добавлена проверка равенства сортированных объектов при редактировании контакта и группы

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -5,7 +5,7 @@ def test_edit_first_contact(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname="Contact for edit"))
     old_contacts = app.contact.get_contact_list()
-    app.contact.edit_first_contact(Contact(firstname="NewFirstname",
+    contact = Contact(firstname="NewFirstname",
         middlename="NewMiddlename",
         lastname="NewLastname",
         nickname="NewNickname",
@@ -29,6 +29,10 @@ def test_edit_first_contact(app):
         ayear="2011",
         address2="NewAddress2",
         phone2="114",
-        notes="NewNotes"))
+        notes="NewNotes")
+    contact.id = old_contacts[0].id
+    app.contact.edit_first_contact(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
