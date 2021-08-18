ec38b936d0a45a73e61a44e126a970ab4a58c210
sash <softester@yandex.ru>
2021-08-18 08:55:56 +0300

    Добавлено кеширование и хеширование при загрузке списка групп

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -6,6 +6,6 @@ def test_delete_first_group(app):
         app.group.create(Group(name="Group for delete"))
     old_groups = app.group.get_group_list()
     app.group.delete_first_group()
+    assert len(old_groups) - 1 == app.group.count()
     new_groups = app.group.get_group_list()
-    assert len(old_groups) - 1 == len(new_groups)
     assert old_groups[1:] == new_groups
