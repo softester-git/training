abff233bf9e94cca1ba71fee05930769fbcbbf7b
sash <softester@yandex.ru>
2021-09-08 12:37:35 +0300

    Параметры командной строки вынесены в конфигурационный файл. Добавлена оптимизация загрузки файла

--- conftest.py
+++ conftest.py
@@ -1,21 +1,23 @@
 import pytest
+import json
 from fixture.application import Application
 
 
 fixture = None
+target = None
 
 
 @pytest.fixture
 def app(request):
     global fixture
+    global target
     browser = request.config.getoption("--browser")
-    baseUrl = request.config.getoption("--baseUrl")
-    if fixture is None:
-        fixture = Application(browser=browser, baseUrl=baseUrl)
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser, baseUrl=baseUrl)
-    fixture.session.ensure_login(username="admin", password="secret")
+    if target is None:
+        with open(request.config.getoption("--target")) as config_file:
+            target = json.load(config_file)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, baseUrl=target["baseUrl"])
+    fixture.session.ensure_login(username=target["username"], password=target["password"])
     return fixture
 
 
@@ -30,4 +32,4 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--baseUrl", action="store", default="https://localhost/addressbook/index.php")
+    parser.addoption("--target", action="store", default="target.json")
