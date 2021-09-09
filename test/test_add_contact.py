05935d71c05ead0ad60eef7fc5f0c236eac73090
sash <softester@yandex.ru>
2021-09-09 16:02:12 +0300

    Реализована загрузка тестовых данных для групп и контактов из json файла

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -4,36 +4,8 @@ import random
 import pytest
 
 
-testdata = [Contact(firstname="", address="", lastname="")] + [Contact(firstname=ContactHelper.random_string("firstname", 10),
-                                                                          address=ContactHelper.random_string("address", 20),
-                                                                          lastname=ContactHelper.random_string("lastname", 10),
-                                                                          nickname=ContactHelper.random_string("nickname", 10),
-                                                                        middlename=ContactHelper.random_string("middlename", 10),
-                                                                       title=ContactHelper.random_string("title", 10),
-                                                                       home=random.randrange(10),
-                                                                       fax=random.randrange(10),
-                                                                       work=ContactHelper.random_digit(10),
-                                                                       mobile=ContactHelper.random_digit(10),
-                                                                       phone2=ContactHelper.random_digit(10),
-                                                                       email=ContactHelper.random_string("email", 10),
-                                                                       email2=ContactHelper.random_string("email2", 10),
-                                                                       email3=ContactHelper.random_string("email3", 10),
-                                                                       photo="title.gif",
-                                                                       company=ContactHelper.random_string("company", 10),
-                                                                       homepage=ContactHelper.random_string("homepage", 10),
-                                                                       bday=str(random.randrange(1,28)),
-                                                                       bmonth=ContactHelper.random_month(),
-                                                                       byear=random.randrange(1970,2021),
-                                                                       aday=str(random.randrange(1,28)),
-                                                                       amonth=ContactHelper.random_month(),
-                                                                       ayear=random.randrange(1970,2021),
-                                                                       address2=ContactHelper.random_string("address2", 10),
-                                                                       notes=ContactHelper.random_string("notes", 10),
-                                                                       )
-                                                     for i in range(5)]
-
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
+def test_add_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
