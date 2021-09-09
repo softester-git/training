6eafcd96b1906d337ca6ff6d12c09afa962d0300
sash <softester@yandex.ru>
2021-09-09 17:41:17 +0300

    Создана фикстура для работы с БД

--- conftest.py
+++ conftest.py
@@ -4,26 +4,43 @@ import jsonpickle
 import os.path
 import importlib
 from fixture.application import Application
+from fixture.db import DbFixture
 
 
 fixture = None
 target = None
 
 
+def load_config(file):
+    global target
+    if target is None:
+        root_path = os.path.dirname(os.path.abspath(__file__))
+        config_file = os.path.join(root_path, file)
+        with open(config_file) as f:
+            target = json.load(f)
+    return(target)
+
+
 @pytest.fixture
 def app(request):
     global fixture
-    global target
+    root_path = os.path.dirname(os.path.abspath(__file__))
+    web_config = load_config(request.config.getoption("--target"))["web"]
     browser = request.config.getoption("--browser")
-    if target is None:
-        root_path = os.path.dirname(os.path.abspath(__file__))
-        config_file = os.path.join(root_path, request.config.getoption("--target"))
-        with open(config_file) as f:
-            target = json.load(f)
     if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=browser, baseUrl=target["baseUrl"], photoPath=root_path)
-    fixture.session.ensure_login(username=target["username"], password=target["password"])
+        fixture = Application(browser=browser, baseUrl=web_config["baseUrl"], photoPath=root_path)
+    fixture.session.ensure_login(username=web_config["username"], password=web_config["password"])
     return fixture
+
+
+@pytest.fixture(scope="session")
+def db(request):
+    db_config = load_config(request.config.getoption("--target"))["db"]
+    dbfixture = DbFixture(host=db_config["host"], name=db_config["name"], user=db_config["user"], password=db_config["password"])
+    def fin():
+        dbfixture.destroy()
+    request.addfinalizer(fin)
+    return(dbfixture)
 
 
 @pytest.fixture(scope="session", autouse=True)
