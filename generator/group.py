8b68c2267faf7bab7ee8850680c916a68397cf01
sash <softester@yandex.ru>
2021-09-09 15:07:04 +0300

    Создан генератор тестовых данных в json файл для групп

--- generator/group.py
+++ generator/group.py
@@ -1,7 +1,7 @@
 from model.group import Group
 from fixture.group import GroupHelper
 import os.path
-import json
+import jsonpickle
 import getopt
 import sys
 
@@ -22,4 +22,5 @@ testdata = [Group(name="", header="", footer="")] + [Group(name=GroupHelper.rand
                                                      for i in range(n)]
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 with open(file, "w") as fd:
-    fd.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+    jsonpickle.set_encoder_options("json", indent=2)
+    fd.write(jsonpickle.encode(testdata))
