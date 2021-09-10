11216b41854a114a471280a61024b14090817af7
sash <softester@yandex.ru>
2021-09-10 04:29:58 +0300

    Добавлены замеры времени выполнения функций

--- test/test_db_matches_ui.py
+++ test/test_db_matches_ui.py
@@ -1,9 +1,11 @@
 from model.group import Group
+from timeit import timeit
 
 
 def test_group_list(app, db):
-    ui_list = app.group.get_group_list()
+    print(timeit(lambda: app.group.get_group_list()))
     def clear(group):
         return(Group(id=group.id, name=group.name.strip()))
-    db_list = map(clear, db.get_group_list())
-    assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
+    print(timeit(lambda: map(clear, db.get_group_list()), number=1000))
+    assert False
+    #assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
