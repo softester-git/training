5e56a1cb76c0eb5852578db1fb95f17e42dca2e3
sash <softester@yandex.ru>
2021-09-10 05:15:51 +0300

    Исправлен тест удаления группы на работу с БД

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,7 +5,6 @@ def test_add_group(app, db, json_groups):
     group = json_groups
     old_groups = db.get_group_list()
     app.group.create(group)
-    assert len(old_groups) + 1 == app.group.count()
     new_groups = db.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
