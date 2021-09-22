ea0c5b001a48db9d0f6ff9c32d389df0b7be5a7b
sash <softester@yandex.ru>
2021-09-22 21:40:33 +0300

    Проверки для контактов реализованы через БД

--- test/test_check_contacts.py
+++ test/test_check_contacts.py
@@ -1,12 +1,11 @@
-from random import randrange
-
-
-def test_check_contact_phones(app):
-    contacts = app.contact.get_contact_list()
-    index = randrange(len(contacts))
-    contact_from_edit_page = app.contact.get_contact_from_edit_page(index)
-    assert contacts[index].all_emails_from_home == app.merge_emails_like_on_home_page(contact_from_edit_page)
-    assert contacts[index].all_phones_from_home == app.merge_phones_like_on_home_page(contact_from_edit_page)
-    assert contacts[index].address == app.merge_text_like_on_home_page(contact_from_edit_page.address)
-    assert contacts[index].firstname == app.merge_text_like_on_home_page(contact_from_edit_page.firstname)
-    assert contacts[index].lastname == app.merge_text_like_on_home_page(contact_from_edit_page.lastname)
+def test_contact_home_page_eq_edit_page(app, db):
+    contacts = db.get_contact_list()
+    contacts_home = app.contact.get_contact_list()
+    for home_record in contacts_home:
+        db_record = list(filter(lambda x: x.id == home_record.id, contacts))[0]
+        assert str(db_record.fname).strip() == str(home_record.fname).strip()
+        assert str(db_record.lname).strip() == str(home_record.lname).strip()
+        #home_record.all_emails = "None\nNone\nNone" if home_record.all_emails == None else home_record.all_emails
+        assert db_record.all_emails == home_record.all_emails
+        #home_record.all_phones = "None\nNone\nNone\nNone" if home_record.all_phones == None else home_record.all_phones
+        assert db_record.all_phones == home_record.all_phones
