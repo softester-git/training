f0bb8044f971805b2545918a495427e6ae228264
sash <softester@yandex.ru>
2021-09-28 13:37:13 +0300

    Добавлено создание группы или контакта при их отсутствии

--- test/test_add_contact_to_group.py
+++ test/test_add_contact_to_group.py
@@ -1,19 +1,17 @@
 import random
+from model.contact import Contact
+from model.group import Group
 
 
 def test_add_contact_to_group(db, app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="Contact for add to group"))
     contacts = db.get_contact_list()
+    if app.group.count() == 0:
+        app.group.create(Group(name="group for contact"))
     groups = db.get_group_list()
-    try:
-        random_contact = random.choice(contacts)
-    except:
-        print("No contacts")
-        return
-    try:
-        random_group = random.choice(groups)
-    except:
-        print("No groups")
-        return
+    random_contact = random.choice(contacts)
+    random_group = random.choice(groups)
     if db.check_relation(group=random_group, contact=random_contact):
         print("Relation already exist")
         return
--- test/test_del_contact_from_group.py
+++ test/test_del_contact_from_group.py
@@ -1,19 +1,17 @@
 import random
+from model.contact import Contact
+from model.group import Group
 
 
 def test_del_contact_from_group(db, app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="Contact for add to group"))
     contacts = db.get_contact_list()
+    if app.group.count() == 0:
+        app.group.create(Group(name="group for contact"))
     groups = db.get_group_list()
-    try:
-        random_contact = random.choice(contacts)
-    except:
-        print("No contacts")
-        return
-    try:
-        random_group = random.choice(groups)
-    except:
-        print("No groups")
-        return
+    random_contact = random.choice(contacts)
+    random_group = random.choice(groups)
     if db.check_relation(group=random_group, contact=random_contact):
         #db.del_relation(group=random_group, contact=random_contact)
         app.contact.del_relation(group=random_group, contact=random_contact)
