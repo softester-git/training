db66e619fc012eed38d98e2f80b94e3cb97fc41c
sash <softester@yandex.ru>
2021-09-29 09:28:45 +0300

    Jenkins

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,10 +1,14 @@
 from model.group import Group
+import pytest
 
 
 def test_add_group(app, db, json_groups, check_ui):
     group = json_groups
-    old_groups = db.get_group_list()
-    app.group.create(group)
+    with pytest.allure.step("Given group list"):
+        old_groups = db.get_group_list()
+    with pytest.allure.step("Create group %s" % group):
+        app.group.create(group)
+
     new_groups = db.get_group_list()
     old_groups.append(Group(name=group.name.strip()))
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
